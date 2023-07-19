9159ce041ef08f41ed1dac8856c03fe6b80759f1
SebaJuarez <sebajuarezz@gmail.com>
2023-07-19 20:03:06 -0300

    ..

--- src/ar/edu/unlu/mancala/modelo/Jugador.java
+++ src/ar/edu/unlu/mancala/modelo/Jugador.java
@@ -5,6 +5,26 @@ import ar.edu.unlu.mancala.vista.JugadorLectura;
 public class Jugador implements JugadorLectura{
 	
 	private String nombre;
+	private int ganadas;
+	private int perdidas;
+	private int empatadas;
+	
+	public int getPerdidas() {
+		return perdidas;
+	}
+
+	public void setPerdidas(int perdidas) {
+		this.perdidas = perdidas;
+	}
+
+	public int getEmpatadas() {
+		return empatadas;
+	}
+
+	public void setEmpatadas(int empatadas) {
+		this.empatadas = empatadas;
+	}
+
 
 	public String getNombre() {
 		return nombre;
@@ -13,7 +33,13 @@ public class Jugador implements JugadorLectura{
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
-	
+
+	public int getGanadas() {
+		return ganadas;
+	}
+
+	public void setGanadas(int ganadas) {
+		this.ganadas = ganadas;
+	}
 	
 }
